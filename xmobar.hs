102ebca77ae406f2f29848cb805010529bb8b9b8
Lars Tingelstad <lars@lars-debian-x201.lan>
2012-08-19 21:08:11 +0200

    Tweaked xmobar.hs

--- xmobar.hs
+++ xmobar.hs
@@ -2,23 +2,21 @@
 -- Author: Vic Fryzel
 -- http://github.com/vicfryzel/xmonad-config
 
--- This is setup for dual 1920x1080 monitors, with the right monitor as primary
 Config {
     font = "xft:Fixed-8",
-    --bgColor = "#000000",
     bgColor = "#2c2c2c",
     fgColor = "#ffffff",
     lowerOnStart = True,
     commands = [
-        Run Weather "ENVA" ["-t","<tempC>C <skyCondition>","-L","10","-H","20","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+	Run Battery ["-t", "Batt: <left>%", "-L","20","-H","50","--high","#CEFFAC","--normal","yellow", "--low", "#FFB6B0"] 10,
+        Run Weather "ENVA" ["-t","<tempC>C","-L","10","-H","20","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+	Run Cpu ["-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10, 
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Network "wlan0" ["-t","wlan0: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%A %b %_d %H:%M" "date" 10,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>   %ENVA%"
+    template = "%StdinReader% }{ %cpu%   %memory%   %wlan0%   %battery%   %date%   %ENVA%" 
 }
